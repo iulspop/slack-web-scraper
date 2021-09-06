4d2ecc0a9b3354a7e7bec040f7a176e64d994a65
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-09-06 18:53:57 -0400

    Add go to channel functionality

--- src/collectData.js
+++ src/collectData.js
@@ -6,7 +6,10 @@ const ensureLogin = require('./utils/ensureLogin');
 (async () => {
   options = {
     headless: true,
-    defaultViewport: null
+    defaultViewport: {
+      height: 2000,
+      width: 1463
+    }
   }
   const browser = await puppeteer.launch(options);
   const page    = await browser.newPage();
@@ -14,6 +17,25 @@ const ensureLogin = require('./utils/ensureLogin');
 
   await page.goto(process.env.SLACK_WORKSPACE_URL);
 
+  await gotoChannel(page, 'the-spot')
+  await page.waitForTimeout(1000)
+
   await page.screenshot({path: './screenshot.png'})
   await browser.close()
 })();
+
+async function gotoChannel(page, channelName) {
+  let channels = await page.$$('.p-channel_sidebar__name')
+  let [channelButton] = await filterByText(page, channels, channelName)
+  await channelButton.click()
+}
+
+async function filterByText(page, elementHandles, textString) {
+  filteredElementHandles = []
+  for (let i = 0; i < elementHandles.length; i++) {
+    let elementHandle = elementHandles[i]
+    let text = await page.evaluate(element => element.textContent, elementHandle)
+    if (text === textString) { filteredElementHandles.push(elementHandle) }
+  }
+  return filteredElementHandles
+}
