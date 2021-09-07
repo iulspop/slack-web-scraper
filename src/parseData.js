f0093510e8af1df8e7c18741735eb4b2d3c6443b
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-09-07 19:37:58 -0400

    fix weeklySignups iterator and fix sortByDateAndCourse

--- src/parseData.js
+++ src/parseData.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const cheerio = require('cheerio')
-const { clearCustomQueryHandlers } = require('puppeteer')
+const { log } = require('console')
 
 let dataPath = path.resolve(__dirname, '../raw.html')
 let channelFeedData = fs.readFileSync(dataPath, 'utf-8')
@@ -12,33 +12,82 @@ let signups = [...weeklySignups(posts)]
 
 console.log(signups)
 
-// let signupsWithData = getCourseSignupReplies(signups)
+signups = sortByDateAndCourse(posts)
+
+let signupsWithData = getCourseSignupReplies(signups)
+
+log(signups)
 
 function* weeklySignups(posts) {
   let thursdayIdx = null
   let thursdayDate = null
-  let nextDayIdx = null
 
   for (let i = 0; i < posts.length; i++) {
     const post = posts[i]
     if (isDayElement(post)) {
-      const $ = cheerio.load(post)
-      const date = $('.c-message_list__day_divider__label__pill').text()
+      const date = getDate(post)
+
+      if (i !== thursdayIdx && thursdayIdx !== null) {
+        yield { date: thursdayDate, posts: posts.slice(thursdayIdx + 1, i).length }
+        thursdayIdx = null
+      }
 
       if (date.match(/Thursday/i)) {
         thursdayIdx = i
         thursdayDate = date
-      } else {
-        nextDayIdx = i
-        if (thursdayIdx) yield { date: thursdayDate, posts: posts.slice(thursdayIdx + 1, nextDayIdx).length }
-        thursdayIdx = null
       }
     }
   }
 }
 
+function sortByDateAndCourse(posts) {
+  const signups = []
+  let date = null
+  let signup = null
+
+  posts.forEach(post => {
+    if (isDayElement(post)) {
+      date = getDate(post)
+    } else if (date.match(/Thursday/i)) {
+      if (post.match(/Ruby 101-109/i)) {
+        signup = weeklySignup(date)
+        signups.push(signup)
+        signup.RB101 = post
+      }
+
+      if (post.match(/Ruby 121-129/i)) {
+        signup.RB120 = post
+      }
+
+      if (post.match(/Ruby 130-139/i)) {
+        signup.RB130 = post
+      }
+    }
+  })
+
+  return signups
+}
+
+function getCourseSignupReplies(signups) {
+  signups.forEach(weeklySignup => {
+    Object.keys(weeklySignup).forEach(key => {
+      if (key === 'date') { return }
+      const data = weeklySignup[key]
+      const $ = cheerio.load(data)
+      const replyCount = $('.c-message__reply_count').text()
+      weeklySignup[key] = replyCount
+    })
+  })
+  return signups
+}
+
+
 function isDayElement(post) {
   return post.match('.c-message_list__day_divider__label__pill"')
+}
+
+function getDate(post) {
+  return cheerio.load(post)('.c-message_list__day_divider__label__pill').text()
 }
 
 
@@ -54,39 +103,6 @@ function check(cases, callbacks, toEvaluate) {
       callback()
     }
   }
-}
-
-function courseRegexes() {
-  return {
-    RB101: /Ruby 101-109/i,
-    JS101: /JS 101-109/i,
-    RB120: /Ruby 121-129/i,
-    JS120: /JS 120-129/i,
-    RB130: /Ruby 130-139/i,
-    JS130: /JS 130-139/i,
-    LS170: /LS 170-171/i,
-    RB175: /Ruby 175/i,
-    JS175: /JS 175/i,
-    LS180: /LS 180-181/i,
-    RB185: /Ruby 185/i,
-    JS185: /JS 185/i,
-    JS210: /& 210-211/i,
-    LS215: /& 225-229/i,
-    JS230: /JS 230-239/i,
-  }
-}
-
-function getCourseSignupReplies(signups) {
-  signups.forEach(weeklySignup => {
-    Object.keys(weeklySignup).forEach(key => {
-      if (key === 'date') { return }
-      const data = weeklySignup[key]
-      const $ = cheerio.load(data)
-      const replyCount = $('.c-message__reply_count').text()
-      weeklySignup[key] = replyCount
-    })
-  })
-  return signups
 }
 
 function weeklySignup(date) {
@@ -110,36 +126,22 @@ function weeklySignup(date) {
   }
 }
 
-function sortByDateAndCourse(courseSignupsAndDays) {
-  const signups = []
-  let currentDate = null
-  let currentWeeklySignup = null
-
-  courseSignupsAndDays.forEach(post => {
-    if (post.match('.c-message_list__day_divider__label__pill"')) {
-      const $ = cheerio.load(post)
-      const date = $('.c-message_list__day_divider__label__pill').text()
-      console.log(date)
-      currentDate = date
-    }
-
-    if (currentDate.match(/Thursday/i)) {
-      currentWeeklySignup = weeklySignup(currentDate)
-      signups.push(currentWeeklySignup)
-
-      if (post.match(/Ruby 101-109/i)) {
-        currentWeeklySignup.RB101 = post
-      }
-
-      if (post.match(/Ruby 121-129/i)) {
-        currentWeeklySignup.RB120 = post
-      }
-
-      if (post.match(/Ruby 130-139/i)) {
-        currentWeeklySignup.RB130 = post
-      }
-    }
-  })
-
-  return signups
+function courseRegexes() {
+  return {
+    RB101: /Ruby 101-109/i,
+    JS101: /JS 101-109/i,
+    RB120: /Ruby 121-129/i,
+    JS120: /JS 120-129/i,
+    RB130: /Ruby 130-139/i,
+    JS130: /JS 130-139/i,
+    LS170: /LS 170-171/i,
+    RB175: /Ruby 175/i,
+    JS175: /JS 175/i,
+    LS180: /LS 180-181/i,
+    RB185: /Ruby 185/i,
+    JS185: /JS 185/i,
+    JS210: /& 210-211/i,
+    LS215: /& 225-229/i,
+    JS230: /JS 230-239/i,
+  }
 }
