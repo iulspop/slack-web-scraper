627e31347301d186074f44673a555d5b1ba2b942
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2022-03-09 08:44:30 -0500

    Change parse prompt to show list of options

--- src/parseData/utils/promptFileToParse.js
+++ src/parseData/utils/promptFileToParse.js
@@ -4,17 +4,16 @@ const inquirer = require('inquirer')
 const SLACK_DATA_FOLDER_PATH = 'slack-data/'
 
 async function promptFileToParse() {
-  const defaultFilePath = await getDefaultFilePath()
-  const { filePath } = await prompt(defaultFilePath)
+  const defaultFilePaths = await getDefaultFilePaths()
+  const { filePath } = await prompt(defaultFilePaths)
   return filePath
 }
 
-async function getDefaultFilePath() {
+async function getDefaultFilePaths() {
   try {
     const fileList = await fs.readdir(SLACK_DATA_FOLDER_PATH)
     const htmlFileList = fileList.filter(filePath => filePath.match(/\.html$/))
-    const mostRecentScrapedDataFile = htmlFileList[htmlFileList.length - 1]
-    return SLACK_DATA_FOLDER_PATH + mostRecentScrapedDataFile
+    return htmlFileList.map(filePath => SLACK_DATA_FOLDER_PATH + filePath)
   } catch (error) {
     console.log("Couldn't load scraped data file list from directory:", SLACK_DATA_FOLDER_PATH)
     console.log('Should run `npm run collect` to scrape Slack data first before parsing.')
@@ -22,23 +21,28 @@ async function getDefaultFilePath() {
   }
 }
 
-async function prompt(defaultFilePath) {
+async function prompt(defaultFilePaths) {
   let questions = {
+    type: 'list',
     name: 'filePath',
-    message: 'Enter the path to the file to parse (defaults to most recent):',
+    message: 'Enter the path to the file to parse:',
+    choices: defaultFilePaths,
     validate: validateFilePath,
   }
+  const defaultFilePath = defaultFilePaths[defaultFilePaths.length - 1]
   if (defaultFilePath) questions = { ...questions, default: defaultFilePath }
   return await inquirer.prompt(questions)
 }
 
 async function validateFilePath(filePath) {
   try {
-    await fs.stat(filePath)
+    const stat = await fs.stat(filePath)
+    if (!stat.isFile()) throw new Error('Not a file')
+    if (!filePath.match(/\.html$/)) throw new Error('Not an HTML file')
     return true
   } catch (error) {
-    console.log('\nFile path is invalid. Please enter a valid path to a scraped data file.')
-    console.error(error)
+    console.log('\n\nFile path is invalid. Please enter a valid path to an HTML file of scraped data.')
+    console.error(error, '\n')
     return false
   }
 }
