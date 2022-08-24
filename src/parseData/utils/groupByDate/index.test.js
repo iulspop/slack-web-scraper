34f401571aef64184e5a088b8cf30e4583ebebd5
William Desportes <williamdes@wdes.fr>
2022-08-24 18:11:25 +0200

    Fix TZ dependent tests

--- src/parseData/utils/groupByDate/index.test.js
+++ src/parseData/utils/groupByDate/index.test.js
@@ -6,12 +6,12 @@ describe('groupByDate', () => {
     let dateGroups = groupByDate(loadTestFile(__dirname, './group-by-date.html'))
     expect(dateGroups).toEqual([
       {
-        timestamp: 'Mon, 29 Aug 2022 04:00:00 GMT',
+        timestamp: 'Mon, 29 Aug 2022 00:00:00 GMT',
         date: 'Monday, August 30th',
         posts: ['<div></div>'],
       },
       {
-        timestamp: 'Thu, 08 Sep 2022 04:00:00 GMT',
+        timestamp: 'Thu, 08 Sep 2022 00:00:00 GMT',
         date: 'Thursday, September 9th',
         posts: ['<div></div>', '<div></div>'],
       },
